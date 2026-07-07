547a1f784640c949c110f0469f09da43a4ea9129
sreehari.ms <iamsreeharims@gmail.com>
2026-07-07 10:15:04 +0530

    pg db connection added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+
+        database: config.get<string>('DB_NAME'),
+
+        autoLoadEntities: true,
+
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
