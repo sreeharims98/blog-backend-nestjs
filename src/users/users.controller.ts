671f6948a6acb92e9db495fc86470362c81f6a3f
sreehari.ms <iamsreeharims@gmail.com>
2026-07-08 09:37:38 +0530

    swagger fields fix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,9 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { User } from './entities/user.entity';
+import { Auth } from 'src/common/decorators/auth.decorator';
 
 @Controller('users')
 export class UsersController {
@@ -15,7 +15,7 @@ export class UsersController {
   }
 
   @Get('me')
-  @UseGuards(JwtAuthGuard)
+  @Auth()
   getProfile(@CurrentUser() user: User) {
     return user;
   }
